365cf1b32bc6095486720d51d24bf4aa10356f13
Reshmalb <reshma.lb27@gmail.com>
2023-08-02 10:28:44 +0530

    signup-serverside

--- backend/controllers/signup.js
+++ backend/controllers/signup.js
@@ -1,5 +1,6 @@
 const path=require('path');
-const Signup= require('../models/signup')
+const Signup= require('../models/signup');
+const { error } = require('console');
 
 exports.userSignup=((req,res,next)=>{
     const username= req.body.username;
@@ -21,3 +22,40 @@ exports.userSignup=((req,res,next)=>{
         console.log(error)
     })
 })
+exports.userSignin=((req,res,next)=>{
+    const username= req.body.username;
+    const password=req.body.password;
+    Signup.findAll({
+        where:{
+            username:username,       
+            password:password
+        }
+        
+    })
+    .then((result)=>{
+        console.log("result",result[0]);
+        res.json({
+            message:" Login successfull",         
+
+        })
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+})
+exports.checkUsername=(req,res,next)=>{
+    const username=req.params.username;
+    Signup.findByPk(username)
+    .then((result)=>{
+        console.log("pk>>>>>.",result);
+        res.json({
+            message:"success ",
+            data:result
+
+        })
+    })
+    .catch(error=>{
+        console.log(error);
+
+    })
+}
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -2,6 +2,11 @@ const express= require('express');
 const router=express.Router();
 const signup=require('../controllers/signup')
 
+
+router.get('/user/:username',signup.checkUsername)
+
 router.post('/user/signup',signup.userSignup)
+router.post('/user/signin',signup.userSignin)
+
 
 module.exports =router;
