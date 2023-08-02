e23139443a2fe24221abb41d0d221d49fc86264b
Reshmalb <reshma.lb27@gmail.com>
2023-08-02 18:51:12 +0530

    signin

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const signup=require('../controllers/signup')
 
 
-router.get('/user/:username',signup.checkUsername)
+// router.get('/user/:username',signup.checkUsername)
 
 router.post('/user/signup',signup.userSignup)
 router.post('/user/signin',signup.userSignin)
